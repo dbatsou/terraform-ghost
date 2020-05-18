a7c85a0465bcdbdcfce531ffed4a001f4df3d829
Dimitrios Batsougiannis <Dimitrios.Batsougiannis@manheim.co.uk>
2020-05-18 18:43:17 +0100

    Pin provider / remove unecessary file

--- backend.tf
+++ backend.tf
@@ -1,8 +1,16 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-storage"
-    storage_account_name = "terraformst"
-    container_name       = "ghost-blog-staging"
+    storage_account_name = "terraformdno"
+    container_name       = "ghost-blog"
     key                  = "staging.terraform.tfstate"
   }
+
+
+
 }
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
